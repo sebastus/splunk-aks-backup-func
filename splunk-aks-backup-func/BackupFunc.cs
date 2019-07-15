66ee1862a74090e9e23c1b7bd62293d062ae2d56
Greg Oliver <golive@microsoft.com>
2019-07-15 10:31:57 +0100

    environment

--- splunk-aks-backup-func/BackupFunc.cs
+++ splunk-aks-backup-func/BackupFunc.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Diagnostics;
 
 namespace splunk_aks_backup_func
 {
@@ -21,14 +22,37 @@ namespace splunk_aks_backup_func
 
             log.LogInformation($"C# Timer trigger function executing script {scriptFileName} at: {DateTime.Now}");
 
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo(getPowershell());
+            startInfo.Arguments = $" -ExecutionPolicy Unrestricted -File {scriptFilePath}";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
             try
             {
-                process.StartInfo.FileName = getPowershell();
-                process.StartInfo.Arguments = $" -ExecutionPolicy Unrestricted -File {scriptFilePath}";
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
+                startInfo.EnvironmentVariables.Add("AZURE_TENANT_ID", getEnvironmentVariable("AZURE_TENANT_ID"));
+                startInfo.EnvironmentVariables.Add("AZURE_APP_ID", getEnvironmentVariable("AZURE_APP_ID"));
+                startInfo.EnvironmentVariables.Add("AZURE_APP_KEY", getEnvironmentVariable("AZURE_APP_KEY"));
+                startInfo.EnvironmentVariables.Add("AZURE_SUBSCRIPTION_ID", getEnvironmentVariable("AZURE_SUBSCRIPTION_ID"));
+                startInfo.EnvironmentVariables.Add("AKS_RG", getEnvironmentVariable("AKS_RG"));
+                startInfo.EnvironmentVariables.Add("AKS_ASSET_RG", getEnvironmentVariable("AKS_ASSET_RG"));
+                startInfo.EnvironmentVariables.Add("AKS_NAME", getEnvironmentVariable("AKS_NAME"));
+            }
+            catch (ArgumentNullException argumentNull)
+            {
+                log.LogError($"Environment is not set. Required values are: AZURE_TENANT_ID, AZURE_APP_IDAZURE_APP_KEY, AZURE_SUBSCRIPTION_ID, AKS_RG, AKS_ASSET_RG, AKS_NAME");
+                throw argumentNull;
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Error occurred while getting environment: {ex.Message}");
+                throw ex;
+            }
+
+            Process process = new Process();
+            try
+            {
+                process.StartInfo = startInfo;
                 process.Start();
             } catch (Exception ex)
             {
@@ -54,37 +78,42 @@ namespace splunk_aks_backup_func
 
         public static string getFilename(string basename)
         {
-
             var filename = "";
-            var home = getEnvironmentVariable("HOME");
-            if (home.Length == 0)
+            var home = "";
+            try
             {
+                home = getEnvironmentVariable("HOME");
+            }
+            catch (ArgumentNullException argumentNull)
+            {
+                // no "HOME" == dev environment
                 filename = "../../../../" + basename;
             }
-            else
-            {
-                filename = home + "/" + basename;
-            }
+            filename = home + "/" + basename;
             return filename;
         }
 
         public static string getPowershell()
         {
-            var home = getEnvironmentVariable("HOME");
-            if (home.Length == 0)
+            try
             {
-                return "powershell.exe";
-            } else
-            {
-                return "pwsh";
+                var home = getEnvironmentVariable("HOME");
             }
+            catch (ArgumentNullException argumentNull)
+            {
+                // no "HOME" = windows dev environment
+                return "powershell.exe";
+            }
+
+            // assumption = runtime environment = Linux
+            return "pwsh";
         }
 
         public static string getEnvironmentVariable(string name)
         {
             var result = System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
-            if (result == null)
-                return "";
+            if (result == null || result.Length == 0)
+                throw new ArgumentNullException($"Key {name} does not exist in environment variables");
 
             return result;
         }
